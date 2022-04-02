8d5f3f14953e626cf5087864821d365d03d5a1b9
AkshayPandey007 <101665005+AkshayPandey007@users.noreply.github.com>
2022-04-02 23:04:21 +0530

    allData
    KaffiHai!!

---
+++ onePlus9.js
@@ -0,0 +1,312 @@
+var redmiCover =[
+
+    {image : "https://images.bewakoof.com/t320/skrlx-oneplus-9-mobile-cover-oneplus-9-mobile-covers-373150-1624516279.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "269",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/floyd-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374879-1624968837.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/hp-glasses-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374904-1624968861.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "299",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/ps4-buttons-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374916-1624968873.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "229",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    
+    {image : "https://images.bewakoof.com/t320/mask-man-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374902-1624968859.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "239",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/iron-man-shadows-oneplus-9-mobile-cover-oneplus-9-mobile-covers-373155-1624516284.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/cloudy-oneplus-9-mobile-cover-oneplus-9-mobile-covers-373778-1624625191.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/naru-oneplus-9-mobile-cover-471145-1643962577-1.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "269",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    
+    {image : "https://images.bewakoof.com/t320/joint-family-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374873-1624968831.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "279",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    
+    {image : "https://images.bewakoof.com/t320/mjolnir-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374876-1624968834.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "219",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    
+    {image : "https://images.bewakoof.com/t320/night-star-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374885-1624968843.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    
+    {image : "https://images.bewakoof.com/t320/never-give-up-oneplus-9-mobile-cover-oneplus-9-mobile-covers-374883-1624968841.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "259",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/one-love-oneplus-9r-mobile-cover-470779-1643962720-1.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "289",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/naru-oneplus-9r-mobile-cover-471216-1643962674-1.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "269",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/hina-oneplus-9r-mobile-cover-471360-1643961951-1.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "259",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/teak-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373328-1624516443.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/pastel-music-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373336-1624516450.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/headphone-jack-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373339-1624516453.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "279",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/wink-new-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373341-1624516455.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/music-panda-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373358-1624516469.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "239",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/tropical-leaves-hope-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373357-1624516468.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/black-marble-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373969-1624625378.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/sleepy-head-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373361-1624516471.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/colourful-mellow-oneplus-9r-mobile-cover-oneplus-9r-mobile-covers-373363-1624516473.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/watermelons-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-347095-1617878760.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/mjolnir-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-347097-1617878765.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/no-excuses-stripe-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-359415-1620679491.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/chibi-mix-potter-xiaomi-redmi-9-power-mobile-cover-hp-xiaomi-redmi-9-power-mobile-covers-359441-1620679540.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/thor-hammer-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-347149-1617878844.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/black-marble-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-346535-1617711270.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/cloudy-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-346536-1620679354.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/wood-compass-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-347109-1617878791.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/leaf-pattern-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-359394-1620679463.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/strength-fist-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-359395-1620679464.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/lep-xiaomi-redmi-9-power-mobile-cover-xiaomi-redmi-9-power-mobile-covers-347105-1620679355.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+    
+    {image : "https://images.bewakoof.com/t320/venom-xiaomi-redmi-9-power-mobile-cover-spl-xiaomi-redmi-9-power-mobile-covers-345494-1620679431.jpg",
+    name : "Metal Gear Blue Xiaomi Redmi 9 Power Mobile Covers",
+    price : "249",
+    strikePrice : 499,
+    speicalPrice : "229 For TriBe Members",
+    },
+     
+    
+    
+    
+    
+    ];
+
+    var data = []
+    
+    redmiCover.map(function(el)
+    {
+        var box = document.createElement("div")
+        box.setAttribute("id","box")
+        box.addEventListener("click", function(){
+            dibbaClick(el)
+        })
+    
+        
+       var  image = document.createElement("img")
+        image.src=el.image;
+        image.style="width:100%"
+    
+        var name = document.createElement("p")
+       name.innerText=el.name;
+       name.setAttribute("id","name")
+    
+       var miniBox = document.createElement("div")
+       miniBox.setAttribute("id","inner")
+    
+       var price = document.createElement("p")
+       price.innerText=el.price
+       price.setAttribute("id","paisa")
+    
+       var discount = document.createElement("p")
+       discount.innerText=el.strikePrice;
+       discount.style="text-decoration: line-through;color:gray";
+    
+       var special = document.createElement("p")
+       special.innerText=el.speicalPrice;
+       special.setAttribute("id","special")
+    
+       
+       miniBox.append(price,discount)
+    
+       box.append(image,name,miniBox,special)
+       document.querySelector("#container").append(box)
+    });
+
+    function dibbaClick(el)
+{
+data.push(el)
+localStorage.setItem("clickData",JSON.stringify(data))
+window.location.href="clickData.html"
+}
